71fb38f88f13dcd4e3613ec6213b49603402932e
lenaic <lenny@lenaics-mbp.home>
2017-12-21 18:51:25 +0100

    fir commit

---
+++ GifGenerator/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Drawing;
+using Gifed;
+
+namespace GifGenerator
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+			var img1 = new Bitmap(8, 11);
+			var img2 = new Bitmap(8, 11);
+			var img3 = new Bitmap(8, 11);
+			var img4 = new Bitmap(8, 11);
+
+			SetPixels(img1, Color.Red);
+			SetPixels(img2, Color.Blue);
+			SetPixels(img3, Color.Orange);
+			SetPixels(img4, Color.Green);
+
+
+			using (var gif = new AnimatedGif(0))
+			{
+				gif.AddFrame(img1, 100);
+				gif.AddFrame(img2, 200);
+				gif.AddFrame(img3, 300);
+				gif.AddFrame(img4, 400);
+				gif.Save("test.gif");
+			}
+
+		}
+
+		static void SetPixels(Bitmap img, Color color)
+		{
+			img.SetPixel(0, 0, color);
+			img.SetPixel(0, 1, color);
+			img.SetPixel(0, 2, color);
+			img.SetPixel(0, 3, color);
+			img.SetPixel(0, 4, color);
+		}
+	}
+}
